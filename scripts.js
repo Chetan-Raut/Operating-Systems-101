a0eda295b0a04239a18c3c333849c20d54bd9a60
Chetan Raut <Chetan.raut2009@gmail.com>
2023-08-24 17:39:56 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+/*Like and Dislike buttons script*/
+
 document.addEventListener("DOMContentLoaded", function () {
   const likeButton = document.getElementById("like-button");
   const dislikeButton = document.getElementById("dislike-button");
@@ -29,3 +31,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+/*Search functionality*/
+
+  const searchForm = document.getElementById('searchForm');
+  const searchInput = document.getElementById('searchInput');
+  const contentItems = document.querySelectorAll('.topic'); // Adjust the selector based on your content structure
+
+  document.getElementById('searchButton').addEventListener('click', function () {
+    const searchQuery = searchInput.value.toLowerCase();
+
+    contentItems.forEach(item => {
+      const itemTitle = item.querySelector('.topic-title').textContent.toLowerCase();
+      const itemText = item.querySelector('p').textContent.toLowerCase();
+
+      if (itemTitle.includes(searchQuery) || itemText.includes(searchQuery)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+
+  // Prevent default form submission
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
